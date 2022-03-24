e568bf6897a122980c4d2e22a5603b46a8fac539
55499 <55499@DUDAZANOTTO>
2022-03-24 17:28:16 -0300

    Add project files.

---
+++ PeriodosAtras.ConsoleApp4/Contador.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeriodosAtras.ConsoleApp4
+{
+    internal class Contador
+    {
+
+        public void DataCalculos()
+        {
+            DateTime DataAtual = DateTime.Now;
+            string Data1dia = DateTime.Now.AddDays(-1).ToString("dd/MM/yyyy");
+            Console.WriteLine(" Há um dia atras");
+            DateTime.Now.AddDays(-1);
+        }
+        public void DataCalculos2()
+        {
+            string Data2d = DateTime.Now.AddDays(-2).ToString("dd/MM/yyyy");
+            Console.WriteLine("Há dois dias atras");
+        }
+        public void DataCalculos3()
+        {
+            string Data3d = DateTime.Now.AddDays(-7).ToString("dd/MM/yyyy");
+            Console.WriteLine("Há uma semana atras");
+        }
+        public void DataCalculos4()
+        {
+            string Data2m = DateTime.Now.AddMonths(-2).ToString("dd/MM/yyyy");
+            Console.WriteLine("Há dois meses atras");
+        }
+
+            string Data10a = DateTime.Now.AddYears(10).ToString("dd/MM/yyyy");
+        
+
+        
+
+
+    }
+}
---
+++ PeriodosAtras.ConsoleApp4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace PeriodosAtras.ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+           
+        
+           
+            TimeSpan dias;
+            Contador contador = new Contador();
+
+            string datadeentrada;
+            DateTime dtDatas;
+
+            Console.WriteLine("Nossa data atual é dia 24/03/2022, coloque uma data qualquer e vamos ver  quantos dias está");
+
+
+            datadeentrada = Console.ReadLine();
+
+            dtDatas = DateTime.Parse(datadeentrada);
+
+            Console.WriteLine("Confirma a data digitada? " + dtDatas);
+
+
+            
+            Console.ReadKey();
+            
+            dias = DateTime.Now - dtDatas;
+            if (dias.Days < 7)
+            {
+                Console.WriteLine(dias.Days + " dias atras ");
+            }
+            else if (dias.Days < 30)
+            {
+                int semanas = dias.Days / 7;
+                Console.WriteLine("foi a " + semanas + " semana atras");
+
+            }
+            else if (dias.Days < 360)
+            {
+                int mes = dias.Days / 30;
+                Console.WriteLine("foi a" + mes + "meses e a " + semanas + "semanas");
+            }
+
+
+
+
+        }
+        
+
+    }
+}
